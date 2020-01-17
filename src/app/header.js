77c519a8afb971ac4ed4b5acb70f90e7b829df68
amellalkamel <amellalkameleddine@gmail.com>
2020-01-17 20:17:58 +0100

    develop branch

--- src/app/header.js
+++ src/app/header.js
@@ -3,7 +3,7 @@ export class Header
 	constructor()
 	{ 
 	console.log(`This is header constructor page`);
-	console.log(`This is header constructor page`); 
+	console.log(`je suis sur la branch develop`); 
 
 	} 
 	getFirstHeading()
